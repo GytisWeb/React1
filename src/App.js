0c938d0077a9054f0c2996818bdd8d9f370b5368
Moksleivis <Moksleivis@kitm.local>
2023-05-25 13:09:11 +0300

    laba diena

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import BooksList from "./components/books/BooksList";
+import Header from "./components/Header/Header";
+import Counter from "./components/counter/Counter";
+
+function App() {
+  return (
+    <div>
+    <Header/>
+    <Counter/>
+    <BooksList/>
+    
+    </div>
+  );
+}
+
+export default App;
+
+
+
